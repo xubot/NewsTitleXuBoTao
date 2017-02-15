8e01aad2db50209efbcacf36f0224363bbbc3fc3
maura <13051672112@163.com>
2017-02-15 10:17:26 +0800

    修改

---
+++ app/src/main/java/com/example/administrator/newstitlexubotao/Uilt/Xutils.java
@@ -0,0 +1,13 @@
+package com.example.administrator.newstitlexubotao.Uilt;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+public class Xutils extends Application {
+    @Override
+    public void onCreate() {
+        //初始化xutils
+    }
+}
--- app/src/main/java/com/example/administrator/newstitlexubotao/fragment/Message_Fragment_Title.java
+++ app/src/main/java/com/example/administrator/newstitlexubotao/fragment/Message_Fragment_Title.java
@@ -1,11 +1,13 @@
 package com.example.administrator.newstitlexubotao.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.administrator.newstitlexubotao.R;
 
@@ -18,6 +20,24 @@ public class Message_Fragment_Title extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View inflate = inflater.inflate(R.layout.message_fragment_title, null);
+        ListView listView= (ListView) inflate.findViewById(R.id.message_Listview);
         return inflate;
+    }
+    class Task extends AsyncTask<String ,String ,String>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
